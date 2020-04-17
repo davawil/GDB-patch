f6eb56678b8bde3bce763e123a8f0c16512088b4
kurtaa1 <davwil@student.chalmers.se>
2020-04-17 22:51:19 +0200

    Logistical version
    Added a version handling more logistics. Might not be worth the hastle

--- gdb/sparc-linux-nat.c
+++ gdb/sparc-linux-nat.c
@@ -64,18 +64,74 @@ fill_fpregset (const struct regcache *regcache,
 /*Added by David Wilkins----->*/
 
 struct sparc_linux_hw_breakpoint{
+	//add GDB high level number for reference?
 	unsigned int address;
 	enum target_hw_bp_type type;
-	int execute;
-	//add control bits
+	int enabled;
+	int hw_slot;
+	int inserted;		//can be ignored, remove
 };
-
+static int
+hw_breakpoint_equal(struct sparc_linux_hw_breakpoint bp1, struct sparc_linux_hw_breakpoint bp2){
+	return bp1.address == bp2.address && bp1.type == bp2.type;
+}
+#define SPARC_MAX_HW_BPS 4
+/*stores information about the hardware breakpoints associated with a certain inferior
+ * shall contain inserted breakpoints only*/
+typedef struct sparc_linux_inferior_bps{
+	ptid_t id;						//the inferior id that this structure stores information for (contains pid, lwp, tid)
+	struct sparc_linux_hw_breakpoint *bps[SPARC_MAX_HW_BPS];
+	int num;
+} *inf_bp_list;
+//define vector type for inf_bp_list pointer
+DEF_VEC_P(inf_bp_list);
+//construct and empty inf_bp_list vector
+VEC(inf_bp_list) *inf_list = NULL;
+/*returns the breakpoint that equals bp in the inferior list. If none exist, return null*/
+static struct sparc_linux_hw_breakpoint *
+get_breakpoint_in_inf_list(struct sparc_linux_inferior_bps * list, struct sparc_linux_hw_breakpoint bp){
+	int i;
+	for(i=0; i<SPARC_MAX_HW_BPS; i++){
+		if(list->bps[i] != NULL){
+			if(hw_breakpoint_equal(*list->bps[i], bp))
+				return list->bps[i];
+		}
+	}
+	return NULL;
+}
+/*return 0 on succes*/
+static int
+insert_breakpoint_in_inf_list(struct sparc_linux_inferior_bps * list, struct sparc_linux_hw_breakpoint *bp){
+	int i;
+	for(i = 0; SPARC_MAX_HW_BPS; i++){
+		if(list->bps[i] == NULL){
+			list->bps[i] = bp;
+			return 0;
+		}
+	}
+	return -1;
+}
+static int
+remove_breakpoint_in_inf_list(struct sparc_linux_inferior_bps * list, struct sparc_linux_hw_breakpoint *bp){
+	int i;
+	for(i = 0; SPARC_MAX_HW_BPS; i++){
+		if(list->bps[i] == bp){
+			list->bps[i] = NULL;
+			return 0;
+		}
+	}
+	return -1;
+}
 static int sparc_linux_can_use_hw_breakpoint (struct target_ops *,
 				     enum bptype, int, int);
 static 
 int sparc_linux_can_use_hw_breakpoint (struct target_ops *t,
-			enum bptype arg1, int arg2, int arg3)
+			enum bptype type, int cnt, int othertype)
 {
+	
+	/*check if cnt exceeds max and check hardware registers
+	 * cnt = how many hardware breakpoints are used, othertype is unused (=0)*/
+	
 	printf("can use\n");
 		return 1;				 
 }
@@ -85,26 +141,73 @@ static CORE_ADDR sparc_place_addr(CORE_ADDR addr){
 	//clear two LSBs
 	return (CORE_ADDR)(int_addr & 0xfffffffc);
 }
+
+/*
+static int
+update_bp_registers (struct lwp_info *lwp, void *arg){
+	int i = 0;
+	if(lwp->arch_private == NULL){
+		lwp->arch_private = XCNEW(struct arch_lwp_info);
+	}
+	return 0;
+}
+ * */
+/*return sparc_linux_inferior_bps associated with id. If none exist create a new one*/
+static struct sparc_linux_inferior_bps*
+sparc_linux_get_inferior_bps (ptid_t id){
+  int i;
+  struct sparc_linux_inferior_bps *inf_bps;
+  //iterate through inf_list
+  for (i = 0; VEC_iterate(inf_bp_list, inf_list, i, inf_bps); i++){
+	  if(ptid_equal(inf_bps->id, id))
+		return inf_bps;
+  }
+  //if none exists create a new bp list for the inferior
+  inf_bps = (struct sparc_linux_inferior_bps *) xcalloc (1,sizeof (struct sparc_linux_inferior_bps));
+  inf_bps->id = id; 
+  inf_bps->num = 0;
+  //push to vector
+  VEC_safe_push(inf_bp_list, inf_list , inf_bps);
+  return inf_bps;
+}
 static int sparc_linux_insert_hw_breakpoint (struct target_ops *,
 				     struct gdbarch *, struct bp_target_info *);
 static 
 int sparc_linux_insert_hw_breakpoint (struct target_ops *ops,
 				     struct gdbarch *arch, struct bp_target_info *info)
 {
-	/*TODO: check if */
-  struct sparc_linux_hw_breakpoint bp;
+  struct sparc_linux_hw_breakpoint *bp;
+  struct sparc_linux_inferior_bps *inf_bps;		//breakpoints list for this inferior
+  CORE_ADDR address;
   int inf_pid;
   int r = 1;
-  CORE_ADDR address = sparc_place_addr(info->reqstd_address);
+  int i;
+  //allocate the new breakpoint
+  bp = (struct sparc_linux_hw_breakpoint *) xcalloc (1, sizeof (struct sparc_linux_hw_breakpoint));
+  //get bps list for this inferior
+  inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
+  //place address so it aligns
+  address = sparc_place_addr(info->reqstd_address);
   info->placed_address = address;
-  //info->placed_size = 4;			//unsure if correct
-  bp.address = (unsigned int) address;
-  //bp.type = hw_execute;
+  //build bp structure
+  bp->address = (unsigned int)address;
+  bp->type = hw_execute;
+  bp->enabled = 1;					//assuming is enabled for now
   inf_pid = ptid_get_pid(inferior_ptid);
-  printf("insert (%x) pid: %d\n", (unsigned int)address, inf_pid);
+  //check if breakpoint already exists
+  if(get_breakpoint_in_inf_list(inf_bps, *bp) != NULL)
+	return r;
+  printf("insert (%x) pid: %d\n", bp->address, inf_pid);
   //use address br_address and type as data
-  r = ptrace(PTRACE_SETHBREGS, inf_pid, bp.address, hw_execute);
-  return r;
+  r = ptrace(PTRACE_SETHBREGS, inf_pid, bp->address, hw_execute);
+  bp->hw_slot = r;
+  //if insertion unsuccessful, deallocate. Else place bp in list
+  if(r < 0){
+	  xfree(bp);
+	  return -1;
+  }
+   insert_breakpoint_in_inf_list(inf_bps, bp);
+   return 0;
 }
 
 static int sparc_linux_remove_hw_breakpoint (struct target_ops *,
@@ -113,23 +216,60 @@ static
 int sparc_linux_remove_hw_breakpoint (struct target_ops *ops,
 				     struct gdbarch *arch, struct bp_target_info *info)
 {
-	/*make changes to info fields?*/
+	struct sparc_linux_hw_breakpoint comp_bp; 			//comparator breakpoint
+	struct sparc_linux_hw_breakpoint *bp;
+	struct sparc_linux_inferior_bps *inf_bps;		//breakpoints list for this inferior
+
 	int inf_pid;
 	unsigned int address = (unsigned int) info->placed_address;
+	//get bps list for this inferior
+	inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
+	comp_bp.address = address;
+	comp_bp.type = hw_execute;
 	printf("remove (%x) pid: %d\n", address, inf_pid);
 	inf_pid = ptid_get_pid(inferior_ptid);
-	
-	ptrace(PTRACE_SETHBREGS, inf_pid, address , 4);
+	bp = get_breakpoint_in_inf_list(inf_bps, comp_bp);
+	if(bp == NULL){
+		printf("could not remove: breakpoint not found\n");
+		return 1;
+	}
+	ptrace(PTRACE_SETHBREGS, inf_pid, bp->hw_slot , 4);
+	remove_breakpoint_in_inf_list(inf_bps, bp);
+	xfree(bp);	
 	return 0;				 
 }
 
 static void
 sparc_linux_prepare_to_resume (struct lwp_info *lwp){
+	/*Om det finns någon referens-koppling mellan min låg-nivå lista av hw bps (i GDB) och hög-nivå data strukturen för breakpoints (struct breakpoint) kanske delete remove problemet kan lösas
+genom att i "sparc_linux_prepare_to_resume" kolla om hög-nivå breakpointen finns kvar och om inte, ta bort dess låg-nivå respresentation. */
 	printf("prepare to resume\n");
 }
+/*remove all breakpoints in inferior (pid is pid_t of inferior)*/
 static void
 sparc_linux_forget_process (pid_t pid){
-	/*remove breakpoints */
+	
+	struct sparc_linux_inferior_bps *inf_bps, *p;
+	int i, j;
+	for (i = 0; VEC_iterate(inf_bp_list, inf_list, i, p); i++){
+	  if(p->id.pid == pid){
+		  inf_bps = p;
+		  break;
+	  }
+		
+	}
+	//deallocate all the breakpoints
+	for(j = 0; j <SPARC_MAX_HW_BPS; j++){
+		if(inf_bps->bps[j] != NULL){
+			ptrace(PTRACE_SETHBREGS, getpid(), inf_bps->bps[j]->hw_slot , 4);	//current pid is used since inferior is dead
+			xfree(inf_bps->bps[j]);
+			inf_bps->bps[j] = NULL;
+		}
+	}
+	//deallocate array
+	//xfree(inf_bps->bps);
+	//deallocate bp_list
+	VEC_ordered_remove(inf_bp_list, inf_list,  i);
 	printf("forget process\n");
 }
 

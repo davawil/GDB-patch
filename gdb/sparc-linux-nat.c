09ec78f2d6a0c31e991b8c6a2165bd401f68fdd8
kurtaa1 <davwil@student.chalmers.se>
2020-04-30 10:33:50 +0200

    Update sparc-linux-nat.c

--- gdb/sparc-linux-nat.c
+++ gdb/sparc-linux-nat.c
@@ -35,6 +35,39 @@
 #ifndef PTRACE_SETHBREGS
 #define PTRACE_SETHBREGS 27
 #endif /*PTRACE_SETHBREGS*/
+#ifndef PTRACE_GETHBREGS
+#define PTRACE_GETHBREGS 28
+#endif /*PTRACE_GETHBREGS*/
+
+
+static int sparc_linux_remove_hw_breakpoint (struct target_ops *,
+				     struct gdbarch *, struct bp_target_info *);
+
+static int sparc_linux_insert_hw_breakpoint (struct target_ops *,
+				     struct gdbarch *, struct bp_target_info *);
+					
+static int sparc_linux_remove_watchpoint (struct target_ops *ops, CORE_ADDR addr, int len,
+				 enum target_hw_bp_type type, struct expression *cond);
+				 
+static int sparc_linux_insert_watchpoint (struct target_ops *ops, CORE_ADDR addr, int len,
+				 enum target_hw_bp_type type, struct expression *cond);
+				 
+static int sparc_linux_insert_mask_watchpoint (struct target_ops *ops, CORE_ADDR addr, 
+				  CORE_ADDR mask, enum target_hw_bp_type type);
+				  
+static int sparc_linux_insert_mask_watchpoint (struct target_ops *ops, CORE_ADDR addr, 
+				  CORE_ADDR mask, enum target_hw_bp_type type);
+				  
+static int to_region_ok_for_hw_watchpoint (struct target_ops *ops,
+					   CORE_ADDR addr, int len);
+static int sparc_linux_watchpoint_addr_within_range (struct target_ops *ops,
+					    CORE_ADDR addr, CORE_ADDR start, int len);
+						
+static int sparc_linux_stopped_by_watchpoint (struct target_ops *ops);
+
+int sparc_linux_stopped_data_address (struct target_ops *ops, CORE_ADDR *addr_p);
+
+				 
 
 void
 supply_gregset (struct regcache *regcache, const prgregset_t *gregs)
@@ -66,6 +99,7 @@ fill_fpregset (const struct regcache *regcache,
 struct sparc_linux_hw_breakpoint{
 	//add GDB high level number for reference?
 	unsigned int address;
+	unsigned int mask;
 	enum target_hw_bp_type type;
 	int enabled;
 	int hw_slot;
@@ -122,19 +156,6 @@ remove_breakpoint_in_inf_list(struct sparc_linux_inferior_bps * list, struct spa
 	}
 	return -1;
 }
-static int sparc_linux_can_use_hw_breakpoint (struct target_ops *,
-				     enum bptype, int, int);
-static 
-int sparc_linux_can_use_hw_breakpoint (struct target_ops *t,
-			enum bptype type, int cnt, int othertype)
-{
-	
-	/*check if cnt exceeds max and check hardware registers
-	 * cnt = how many hardware breakpoints are used, othertype is unused (=0)*/
-	
-	printf("can use\n");
-		return 1;				 
-}
 /*place address correctly*/
 static CORE_ADDR sparc_place_addr(CORE_ADDR addr){
 	int int_addr = ((unsigned int) addr);
@@ -170,80 +191,218 @@ sparc_linux_get_inferior_bps (ptid_t id){
   VEC_safe_push(inf_bp_list, inf_list , inf_bps);
   return inf_bps;
 }
-static int sparc_linux_insert_hw_breakpoint (struct target_ops *,
-				     struct gdbarch *, struct bp_target_info *);
+static int sparc_linux_can_use_hw_breakpoint (struct target_ops *,
+				     enum bptype, int, int);
 static 
-int sparc_linux_insert_hw_breakpoint (struct target_ops *ops,
-				     struct gdbarch *arch, struct bp_target_info *info)
+int sparc_linux_can_use_hw_breakpoint (struct target_ops *t,
+			enum bptype type, int cnt, int othertype)
+{
+	/*can not check registers since child isn't created yet*/
+	if(cnt >= SPARC_MAX_HW_BPS)
+		return -1;
+	return 1;			 
+}
+/*if mask equals 0 the default mask will be used*/
+static 
+int sparc_linux_insert_hw_breakpoint_1 (CORE_ADDR address, 
+					  enum target_hw_bp_type type, unsigned int mask)
 {
   struct sparc_linux_hw_breakpoint *bp;
   struct sparc_linux_inferior_bps *inf_bps;		//breakpoints list for this inferior
-  CORE_ADDR address;
   int inf_pid;
-  int r = 1;
+  int r;
+  int r2;
   int i;
+  
+   //get bps list for this inferior
+  inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
   //allocate the new breakpoint
   bp = (struct sparc_linux_hw_breakpoint *) xcalloc (1, sizeof (struct sparc_linux_hw_breakpoint));
-  //get bps list for this inferior
-  inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
-  //place address so it aligns
-  address = sparc_place_addr(info->reqstd_address);
-  info->placed_address = address;
   //build bp structure
   bp->address = (unsigned int)address;
-  bp->type = hw_execute;
+  bp->type = type;
   bp->enabled = 1;					//assuming is enabled for now
   inf_pid = ptid_get_pid(inferior_ptid);
   //check if breakpoint already exists
-  if(get_breakpoint_in_inf_list(inf_bps, *bp) != NULL)
-	return r;
+  if(get_breakpoint_in_inf_list(inf_bps, *bp) != NULL){
+    printf("could not insert: breakpoint already exists\n");
+	goto dealloc;
+  }
+  
+  if(insert_breakpoint_in_inf_list(inf_bps, bp) != 0){
+	  printf("could not insert: breakpoint list full\n");
+	  goto dealloc;
+  }
   printf("insert (%x) pid: %d\n", bp->address, inf_pid);
   //use address br_address and type as data
-  r = ptrace(PTRACE_SETHBREGS, inf_pid, bp->address, hw_execute);
+  r = ptrace(PTRACE_SETHBREGS, inf_pid, bp->address, bp->type);
   bp->hw_slot = r;
   //if insertion unsuccessful, deallocate. Else place bp in list
-  if(r < 0){
-	  xfree(bp);
-	  return -1;
+  if(r < 0)
+	goto dealloc;
+	
+  /*change mask*/
+  if(mask != 0){
+	r2 = ptrace(PTRACE_SETHBREGS, inf_pid, bp->mask, 4 + r);
+	bp->mask = mask;
   }
-   insert_breakpoint_in_inf_list(inf_bps, bp);
-   return 0;
+	
+  return 0;
+  
+  dealloc: xfree(bp);
+  return -1;
+  
+}
+static 
+int sparc_linux_remove_hw_breakpoint_1(struct sparc_linux_hw_breakpoint comp_bp)
+{
+	struct sparc_linux_hw_breakpoint *bp;
+	struct sparc_linux_inferior_bps *inf_bps;		//breakpoints list for this inferior
+	int r = 0;
+	int inf_pid;
+	
+	//get bps list for this inferior
+	inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);	
+	inf_pid = ptid_get_pid(inferior_ptid);
+	printf("remove (%x) pid: %d\n", comp_bp.address, inf_pid);
+	bp = get_breakpoint_in_inf_list(inf_bps, comp_bp);
+	if(bp == NULL){
+		printf("could not remove: breakpoint not found\n");
+		return -1;
+	}
+	r = ptrace(PTRACE_SETHBREGS, inf_pid, bp->hw_slot , 8);
+	remove_breakpoint_in_inf_list(inf_bps, bp);
+	if(r < 0)
+		return -1;
+	xfree(bp);	
+	return 0;				 
 }
 
-static int sparc_linux_remove_hw_breakpoint (struct target_ops *,
-				     struct gdbarch *, struct bp_target_info *);
+
 static 
 int sparc_linux_remove_hw_breakpoint (struct target_ops *ops,
 				     struct gdbarch *arch, struct bp_target_info *info)
 {
 	struct sparc_linux_hw_breakpoint comp_bp; 			//comparator breakpoint
-	struct sparc_linux_hw_breakpoint *bp;
-	struct sparc_linux_inferior_bps *inf_bps;		//breakpoints list for this inferior
-
-	int inf_pid;
-	unsigned int address = (unsigned int) info->placed_address;
-	//get bps list for this inferior
-	inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
-	comp_bp.address = address;
+	comp_bp.address = (unsigned int) info->placed_address;
 	comp_bp.type = hw_execute;
-	printf("remove (%x) pid: %d\n", address, inf_pid);
-	inf_pid = ptid_get_pid(inferior_ptid);
-	bp = get_breakpoint_in_inf_list(inf_bps, comp_bp);
-	if(bp == NULL){
-		printf("could not remove: breakpoint not found\n");
-		return 1;
-	}
-	ptrace(PTRACE_SETHBREGS, inf_pid, bp->hw_slot , 4);
-	remove_breakpoint_in_inf_list(inf_bps, bp);
-	xfree(bp);	
-	return 0;				 
+	return sparc_linux_remove_hw_breakpoint_1(comp_bp);
 }
 
+static 
+int sparc_linux_insert_hw_breakpoint (struct target_ops *ops,
+				     struct gdbarch *arch, struct bp_target_info *info)
+{
+	CORE_ADDR address = sparc_place_addr(info->reqstd_address);
+    info->placed_address = address;
+	return sparc_linux_insert_hw_breakpoint_1(address, hw_execute, 0);
+}
+
+/* Set/clear a hardware watchpoint starting at ADDR, for LEN bytes.
+   TYPE is 0 for write, 1 for read, and 2 for read/write accesses.
+   COND is the expression for its condition, or NULL if there's none.
+   Returns 0 for success, 1 if the watchpoint type is not supported,
+   -1 for failure.  */
+static
+int sparc_linux_remove_watchpoint (struct target_ops *ops, CORE_ADDR addr, int len,
+				 enum target_hw_bp_type type, struct expression *cond)
+{
+  struct sparc_linux_hw_breakpoint comp_bp; 			//comparator breakpoint
+  comp_bp.address = (unsigned int)addr;
+  comp_bp.type = type;
+  return sparc_linux_remove_hw_breakpoint_1(comp_bp);
+}
+
+static			 
+int sparc_linux_insert_watchpoint (struct target_ops *ops, CORE_ADDR addr, int len,
+				 enum target_hw_bp_type type, struct expression *cond)
+{
+  /*TODO: use length to create a mask*/
+  return sparc_linux_insert_hw_breakpoint_1(sparc_place_addr(addr), type, 0);
+}
+
+/* Insert a new masked watchpoint at ADDR using the mask MASK.
+   RW may be hw_read for a read watchpoint, hw_write for a write watchpoint
+   or hw_access for an access watchpoint.  Returns 0 for success, 1 if
+   masked watchpoints are not supported, -1 for failure.  */
+static
+int sparc_linux_insert_mask_watchpoint (struct target_ops *ops, CORE_ADDR addr, 
+				  CORE_ADDR mask, enum target_hw_bp_type type)
+{
+  return sparc_linux_insert_hw_breakpoint_1(sparc_place_addr(addr), type, (unsigned int)mask);
+}
+	  
+	/* Remove a masked watchpoint at ADDR with the mask MASK.
+   RW may be hw_read for a read watchpoint, hw_write for a write watchpoint
+   or hw_access for an access watchpoint.  Returns 0 for success, non-zero
+   for failure.  */
+static
+int sparc_linux_remove_mask_watchpoint (struct target_ops *ops, CORE_ADDR addr, 
+				  CORE_ADDR mask, enum target_hw_bp_type type)
+{
+  struct sparc_linux_hw_breakpoint comp_bp; 			//comparator breakpoint
+  comp_bp.address = (unsigned int)addr;
+  comp_bp.type = type;
+  return sparc_linux_remove_hw_breakpoint_1(comp_bp);
+}
+
+/* Returns the number of debug registers needed to watch the given
+   memory region, or zero if not supported.  */
+static
+int sparc_linux_region_ok_for_hw_watchpoint (struct target_ops *ops,
+					   CORE_ADDR addr, int len)
+{
+	   return 1;
+}
+  
+/* Return non-zero if ADDR is within the range of a watchpoint spanning
+   LENGTH bytes beginning at START.  */
+static
+int sparc_linux_watchpoint_addr_within_range (struct target_ops *ops,
+					    CORE_ADDR addr, CORE_ADDR start, int len)
+{
+			return 1;				
+}
+	  
+/* Returns non-zero if we were stopped by a hardware watchpoint (memory read or
+write).  Only the INFERIOR_PTID task is being queried. */
+static
+int sparc_linux_stopped_by_watchpoint (struct target_ops *ops){
+	CORE_ADDR *addr;
+	return sparc_linux_stopped_data_address(ops, addr);
+}
+	  
+/* Return non-zero if target knows the data address which triggered this
+   target_stopped_by_watchpoint, in such case place it to *ADDR_P.  Only the
+   INFERIOR_PTID task is being queried.  */
+int sparc_linux_stopped_data_address (struct target_ops *ops, CORE_ADDR *addr_p){
+	siginfo_t siginfo;
+	struct sparc_linux_hw_breakpoint comp_bp;
+	struct sparc_linux_hw_breakpoint *bp;
+	struct sparc_linux_inferior_bps *inf_bps;
+	//get siginfo
+	int inf_pid = ptid_get_pid(inferior_ptid);
+	ptrace(PTRACE_GETSIGINFO, inf_pid, 0,&siginfo);
+	//get list if breakpoints 
+	inf_bps = sparc_linux_get_inferior_bps(inferior_ptid);
+	//get get breakpoint that triggered trap
+	comp_bp.address = *((unsigned int *)siginfo.si_addr);
+	comp_bp.type = 0;		//only write watchpoint supported
+	bp = get_breakpoint_in_inf_list(inf_bps, comp_bp);
+	
+	if(bp == NULL)
+		return 0;
+		
+	*addr_p = *((CORE_ADDR *)siginfo.si_addr);
+	return 1;	
+}
+	   
+    
 static void
 sparc_linux_prepare_to_resume (struct lwp_info *lwp){
 	/*Om det finns någon referens-koppling mellan min låg-nivå lista av hw bps (i GDB) och hög-nivå data strukturen för breakpoints (struct breakpoint) kanske delete remove problemet kan lösas
 genom att i "sparc_linux_prepare_to_resume" kolla om hög-nivå breakpointen finns kvar och om inte, ta bort dess låg-nivå respresentation. */
-	printf("prepare to resume\n");
+	//printf("prepare to resume\n");
 }
 /*remove all breakpoints in inferior (pid is pid_t of inferior)*/
 static void
@@ -258,6 +417,8 @@ sparc_linux_forget_process (pid_t pid){
 	  }
 		
 	}
+	if(inf_bps == NULL)
+		return;
 	//deallocate all the breakpoints
 	for(j = 0; j <SPARC_MAX_HW_BPS; j++){
 		if(inf_bps->bps[j] != NULL){
@@ -305,13 +466,22 @@ _initialize_sparc_linux_nat (void)
   t->to_can_use_hw_breakpoint = sparc_linux_can_use_hw_breakpoint; 		//added by David Wilkins
   t->to_insert_hw_breakpoint = sparc_linux_insert_hw_breakpoint; 		//added by David Wilkins
   t->to_remove_hw_breakpoint = sparc_linux_remove_hw_breakpoint; 		//added by David Wilkins
+  t->to_insert_watchpoint = sparc_linux_insert_watchpoint; 				//added by David Wilkins
+  t->to_remove_watchpoint = sparc_linux_remove_watchpoint; 				//added by David Wilkins
+  t->to_insert_mask_watchpoint = sparc_linux_insert_mask_watchpoint; 	//added by David Wilkins
+  t->to_remove_mask_watchpoint = sparc_linux_remove_mask_watchpoint; 	//added by David Wilkins
+  t->to_watchpoint_addr_within_range = sparc_linux_watchpoint_addr_within_range;
+  t->to_region_ok_for_hw_watchpoint = sparc_linux_region_ok_for_hw_watchpoint;
   /* Register the target.  */
   linux_nat_add_target (t);
-  
+  /*override linux implemementation of these operations*/
+  t->to_stopped_data_address = sparc_linux_stopped_data_address;
+  t->to_stopped_by_watchpoint = sparc_linux_stopped_by_watchpoint;
   
   //linux_nat_set_new_thread (t, sparc_linux_new_thread);					//added by David WIlkins
-  linux_nat_set_prepare_to_resume (t, sparc_linux_prepare_to_resume);	//added by David Wilkins
+  //observer_attach_thread_exit(sparc_linux_forget_process);
+  linux_nat_set_prepare_to_resume (t, sparc_linux_prepare_to_resume);		//added by David Wilkins
   
- // linux_nat_set_new_fork (t, sparc_linux_new_fork);						//added by David WIlkins
-  linux_nat_set_forget_process (t, sparc_linux_forget_process);			//added by David Wilkins
+  //linux_nat_set_new_fork (t, sparc_linux_new_fork);						//added by David WIlkins
+  linux_nat_set_forget_process (t, sparc_linux_forget_process);				//added by David Wilkins
 }
